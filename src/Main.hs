f96b569b9934b0cd0fb4e85d6769de4d0d17474e
Danny Gratzer <danny.gratzer@gmail.com>
2014-06-18 21:01:08 -0500

    Main now grabs our feed and prints it

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,23 @@
 module Main where
+import Network.HTTP (getRequest, simpleHTTP, getResponseBody)
+import Control.Exception (try, IOException)
+import Control.Applicative ((<$>))
+import Text.Feed.Import
+import Text.Feed.Types
+import System.IO
+
+urlForFeed :: String
+urlForFeed = "http://haskell.mn/atom.xml"
+
+grabFeedText :: String -> IO (Maybe String)
+grabFeedText url = do
+  rsp <- simpleHTTP (getRequest url)
+  result <- try $ getResponseBody rsp :: IO (Either IOException String)
+  return $ either (const Nothing) Just result
 
 main :: IO ()
-main = putStrLn "Hello HaskellMN"
+main = do
+  result <- flip (>>=) parseFeedString <$> grabFeedText urlForFeed
+  case result of
+    Nothing  -> hPutStrLn stderr $ "No feed found at" ++ urlForFeed
+    Just str -> print str
